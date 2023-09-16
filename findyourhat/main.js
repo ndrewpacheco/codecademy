a2f3c30e76e473d72f1cbe3fc29e7ea9a2e42e74
Andrew Pacheco <ndrewpacheco@gmail.com>
2023-09-15 23:43:23 -0400

    finished game

--- findyourhat/main.js
+++ findyourhat/main.js
@@ -1,22 +1,71 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
 class Field {
   constructor(field) {
-    this.field = field
+    this._field = field;
+    this._currentCoord = [0, 0];
   }
 
   print() {
-    console.log(this.field.join('\n'))
+    console.log("The Field: ");
+    console.log(this._field.join("\n"));
+  }
+
+  playTurn() {
+    let answer;
+    while (!answer) {
+      this.print();
+      answer = prompt("Which way (l,r,d,u)?").toLowerCase();
+      switch (answer) {
+        case "l":
+          this._currentCoord[0] -= 1;
+          break;
+        case "r":
+          this._currentCoord[0] += 1;
+          break;
+        case "d":
+          this._currentCoord[1] += 1;
+          break;
+        case "u":
+          this._currentCoord[1] -= 1;
+          // code block
+          break;
+        default:
+          answer = null;
+          console.log("Invalid answer, try again");
+      }
+    }
+  }
+
+  playGame() {
+    let endGame = false;
+    while (!endGame) {
+      this.playTurn();
+      if (this._field[this._currentCoord[1]][this._currentCoord[0]] === hat) {
+        console.log("you win!!");
+        break;
+      } else if (
+        this._currentCoord.some((n) => n < 0 || n > 2) ||
+        this._field[this._currentCoord[1]][this._currentCoord[0]] == hole
+      ) {
+        console.log("you lose");
+        endGame = true;
+        break;
+      }
+      this._field[this._currentCoord[1]][this._currentCoord[0]] = pathCharacter;
+    }
   }
 }
 
 const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
 ]);
+
+myField.playGame();
